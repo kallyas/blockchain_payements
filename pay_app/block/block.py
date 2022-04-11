45d0d91ce8567b238fc511558f73ec2a3b7a75b5
kallyas <kallyasmedia@gmail.com>
2022-04-11 21:36:31 +0300

    ch: add block class

---
+++ pay_app/block/block.py
@@ -0,0 +1,24 @@
+import hashlib
+import json
+
+
+class Block:
+    def __init__(self, index, data, timestamp, previous_hash):
+        self.index = index
+        self.data = data
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = 0
+
+    def compute_hash(self):
+        """
+        A function that return the hash of the block contents.
+        """
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+    def __repr__(self):
+        """
+        A function that returns a string representation of the Block
+        """
+        return "Block: " + json.dumps(self.__dict__, sort_keys=True)
